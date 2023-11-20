66e45a0a28aca2995ec310c7861fc7bc56c717a9
Gabriel <86722669+idel09@users.noreply.github.com>
2023-11-19 21:48:50 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const images = ['p1.jpg', 'p2.jpg', 'p3.jpg']; // Lista de URLs de las imágenes
+const adImage = document.getElementById('adImage'); // Obtiene la imagen del anuncio
+const adContainer = document.getElementById('adContainer'); // Obtiene el contenedor del anuncio
+let currentIndex = 0; // Índice actual de la imagen en la lista
+function changeImage() {
+  currentIndex = (currentIndex + 1) % images.length; // Cambia al siguiente índice de imagen
+  adImage.src = images[currentIndex]; // Cambia la imagen del anuncio
+}
+// Cambia la imagen cada 5 segundos
+setInterval(changeImage, 1500);
