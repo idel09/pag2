d4aa46d7928233c18c4e1af036ccf0d7d627f4a7
Gabriel <86722669+idel09@users.noreply.github.com>
2025-02-08 19:18:21 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const images = ['p1.jpg','p2.jpg','p3.jpg']; // Lista de URLs de las imágenes
+const images = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg']; // Lista de URLs de las imágenes
 const adImage = document.getElementById('adImage'); // Obtiene la imagen del anuncio
 const adContainer = document.getElementById('adContainer'); // Obtiene el contenedor del anuncio
 let currentIndex = 0; // Índice actual de la imagen en la lista
